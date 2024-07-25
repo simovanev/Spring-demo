e6e9bd2b5ad7773caff121025783ab95877b1f44
Simovanev <simovanev@abv.bg>
2024-07-25 05:36:19 +0300

    Bean Component

--- src/main/java/com/udemy/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/udemy/springcoredemo/common/CricketCoach.java
@@ -10,12 +10,5 @@ public class CricketCoach implements Coach{
     public String dailyWorkout() {
         return "Workout 25 minutes a day.";
     }
-    @PostConstruct
-    public void toDo(){
-        System.out.println("Cricket Coach started " +getClass().getSimpleName());
-    }
-    @PreDestroy
-    public void toDoWhenEnded() {
-        System.out.println("Cricket Coach ended "+getClass().getSimpleName());
-    }
+
 }
---
+++ src/main/java/com/udemy/springcoredemo/common/swimCoach.java
@@ -0,0 +1,8 @@
+package com.udemy.springcoredemo.common;
+
+public class swimCoach implements Coach{
+    @Override
+    public String dailyWorkout() {
+        return "Swim hard";
+    }
+}
